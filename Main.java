2e2390da11429e4ac078d1f34153c2495d045f00
Nandanuness <132527734+Nandanuness@users.noreply.github.com>
2024-07-29 21:27:40 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArvoreBinaria arvore = new ArvoreBinaria();
+        Scanner scanner = new Scanner(System.in);
+        int opcao = 0;
+
+        while (opcao != 5) { // loop continua até que o usuário escolha a op 5
+            System.out.println("\nMenu:");
+            System.out.println("1. Inserir valor");
+            System.out.println("2. Remover valor");
+            System.out.println("3. Exibir árvore em ordem");
+            System.out.println("4. Exibir árvore em pré-ordem");
+            System.out.println("5. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o valor a ser inserido: ");
+                    int valorInserir = scanner.nextInt();
+                    arvore.inserir(valorInserir);
+                    break;
+
+                case 2:
+                    System.out.print("Digite o valor a ser removido: ");
+                    int valorRemover = scanner.nextInt();
+                    arvore.remover(valorRemover);
+                    break;
+
+                case 3:
+                    System.out.println("Árvore em ordem:");
+                    arvore.emOrdem(arvore.getRaiz());
+                    break;
+
+                case 4:
+                    System.out.println("Árvore em pré-ordem:");
+                    arvore.preOrdem(arvore.getRaiz());
+                    break;
+
+                case 5:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+
+        scanner.close();
+    }
+}
