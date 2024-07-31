cf8e17a2b1411c15aeec1c3da2301b802de4482c
Nandanuness <132527734+Nandanuness@users.noreply.github.com>
2024-07-30 21:14:52 -0300

    Add files via upload

--- ArvoreBinaria.java
+++ ArvoreBinaria.java
@@ -9,12 +9,12 @@ public class ArvoreBinaria {
         No novoNo = new No(valor);
         if (this.raiz == null) { // se não houver nenhum nó
             this.raiz = novoNo; // primeiro nó raiz
-        } else { // percorre a árvore para encontrar a posição de inserção
+        } else { // já existe nó, percorre a árvore para encontrar a posição de inserção
             No atual = this.raiz; // começa do nó raiz
             No pai = null; // inicia pai como null
             boolean esquerda = false; // inicia esquerda como false
             while (atual != null) { // percorre até encontrar um nó vazio
-                if (novoNo.getValor() < atual.getValor()) { // se novono é menor que atual, segue a esquerda
+                if (novoNo.getValor() < atual.getValor()) { // se novono é menor que atual, segue a esq
                     pai = atual;
                     atual = atual.getEsq(); // move para esquerda
                     esquerda = true;
@@ -24,9 +24,9 @@ public class ArvoreBinaria {
                     esquerda = false;
                 }
             }
-            if (esquerda) {
+            if (esquerda) { // esquerda true
                 pai.setEsq(novoNo); // add novo nó como filho esquerdo do pai
-            } else {
+            } else { // esquerda false
                 pai.setDir(novoNo); // add novo nó como filho direito do pai
             }
         }
@@ -37,34 +37,34 @@ public class ArvoreBinaria {
     }
 
     public void preOrdem(No no) {
-        if (no == null) {
+        if(no == null) {
             return;
         }
-        System.out.println(no.getValor()); // imprime nó
-        preOrdem(no.getEsq()); // chama recursivamente esquerda
-        preOrdem(no.getDir()); // chama recursivamente direita
+        System.out.println(no.getValor());
+        preOrdem(no.getEsq());
+        preOrdem(no.getDir());
     }
 
     public void emOrdem(No no) {
-        if (no == null) {
+        if(no == null) {
             return;
         }
-        emOrdem(no.getEsq()); // chama recursivamente esquerda
-        System.out.println(no.getValor()); // imprime nó
-        emOrdem(no.getDir()); // chama recursivamente direita
+        emOrdem(no.getEsq());
+        System.out.println(no.getValor());
+        emOrdem(no.getDir());
     }
 
     public void posOrdem(No no) {
-        if (no == null) {
+        if(no == null) {
             return;
         }
-        posOrdem(no.getEsq()); // chama recursivamente esquerda
-        posOrdem(no.getDir()); // chama recursivamente direita
-        System.out.println(no.getValor()); // imprime nó
+        posOrdem(no.getEsq());
+        posOrdem(no.getDir());
+        System.out.println(no.getValor());
     }
 
      void remover(int valor) {
-        raiz = remover(raiz, valor); // atualiza raiz
+        raiz = remover(raiz, valor); // atualiza raiz, se removida ou não
     }
 
     private No remover(No no, int valor) { // recebe nó e valor a ser removido
@@ -76,15 +76,15 @@ public class ArvoreBinaria {
             no.setEsq(remover(no.getEsq(), valor)); // chamada recursiva a esq, atual recebe filho a esq
         } else if (valor > no.getValor()) { // se valor maior que o valor do nó atual, está a direita
             no.setDir(remover(no.getDir(), valor)); // chamada recursiva a dir, atual recebe filho a dir
-        } else { // se valor igual ao nó atual (encontrou o nó a ser removido)
-            if (no.getEsq() == null && no.getDir() == null) { // verifica se o nó nçao tem filhos (folha)
-                return null; // retorna o só null, removendo-o
+        } else { // se valor é igual ao nó atual (encontrou o nó a ser removido)
+            if (no.getEsq() == null && no.getDir() == null) { // verifica se o nó tem filhos (folha)
+                return null; // retorna o nó null, removendo-o
             }
 
-            if (no.getEsq() == null) { // nó a ser removido tem apenas um filho direito
-                return no.getDir(); // se sim, retorna o filho direito para substiruir o nó
-            } else if (no.getDir() == null) { // nó a ser removido tem apenas um filho esquerdo
-                return no.getEsq(); // se sim, retorna o filho esquerdo para substiruir o nó
+            if (no.getEsq() == null) { //se o nó a ser removido tem apenas um filho direito
+                return no.getDir(); // retorna o filho direito para substiruir o nó
+            } else if (no.getDir() == null) { // se o nó a ser removido tem apenas um filho esquerdo
+                return no.getEsq(); // retorna o filho esquerdo para substiruir o nó
             }
 
             No menorNo = encontrarMenorNo(no.getDir()); // encontra o nó de menor valor a direita do atual
@@ -95,7 +95,7 @@ public class ArvoreBinaria {
         return no; // retorna o nó atual após a remoção, atualiza pai para o nó removido
     }
 
-    private No encontrarMenorNo(No no) { // encontra menor nó apartir do nó fornecido
+    private No encontrarMenorNo(No no) { // encontra menor nó apartir do nó fornecido (atual)
         while (no.getEsq() != null) { // enquanto tiver no esquerdo
             no = no.getEsq(); // move para o filho esqerdo
         }
